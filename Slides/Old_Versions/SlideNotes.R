eca34e7c851bcfefd46d557a6011ceaeaab88045
Jill <thomleyje@appstate.edu>
2021-02-04 12:23:03 -0500

    Update SCZ and Boxplot slides

---
+++ Slides/Old_Versions/SlideNotes.R
@@ -0,0 +1,8 @@
+# Update all hypothesis testing slides for new book changes
+# chisquare is in Chapter 10, p vs. pi notation
+
+# Update textbook data, also in package resampledata
+
+# Update modeling slides w/ all the moderndive functions, rearrange
+
+# add shading under curve to normal slides
