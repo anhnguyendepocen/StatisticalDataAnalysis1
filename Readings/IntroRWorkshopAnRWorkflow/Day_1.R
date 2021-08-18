c90e56236468fe114f9f4c49ca087b646177b96d
Jill <thomleyje@appstate.edu>
2021-08-18 16:57:43 -0400

    Moved previous projects

---
+++ Readings/IntroRWorkshopAnRWorkflow/Day_1.R
@@ -0,0 +1,5 @@
+# Day_1.R
+# Reads in some data about Laminaria collected along the Cape Peninsula
+# do various data manipulations, analyses and graphs
+# your name
+# date you created or last worked on the file
